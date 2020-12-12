13b4f3ac1c474f297954cb1a310fe3af57e2c8cf
sainch <santicesanti@hotmail.com>
2020-12-12 12:03:17 -0300

    agregar archivo de funciones.h para el desarrollo de funciones generales

---
+++ funciones.h
@@ -0,0 +1,16 @@
+void gotoxy(int x,int y)
+{  
+      HANDLE hcon;  
+      hcon = GetStdHandle(STD_OUTPUT_HANDLE);  
+      COORD dwPos;  
+      dwPos.X = x;  
+      dwPos.Y= y;  
+      SetConsoleCursorPosition(hcon,dwPos);  
+}
+
+void LimpiarPantalla()
+{
+	printf("\n\n\t\t");
+	system("pause");
+	system("cls");
+}
